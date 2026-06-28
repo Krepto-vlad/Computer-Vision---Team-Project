6c17b6f8dde436fc8d6a6714b0253cb1cc651f53
oCMEXo <artsiomharminovich05@gmail.com>
2026-06-28 23:07:53 +0300

    feat(detect): add mask cleanup and contour-based object detection

---
+++ src/lowlight_cv/detection/__init__.py
@@ -0,0 +1,17 @@
+from lowlight_cv.detection.features import (
+    detect_objects,
+    draw_boxes,
+    fast_detect,
+    harris,
+    orb_detect,
+    shi_tomasi,
+)
+
+__all__ = [
+    "harris",
+    "shi_tomasi",
+    "fast_detect",
+    "orb_detect",
+    "detect_objects",
+    "draw_boxes",
+]
