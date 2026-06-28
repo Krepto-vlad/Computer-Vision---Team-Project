c29862dbaffda65efeab74543c997caf08255593
Uladzisalu <vladshokoladkin@gmail.com>
2026-06-28 22:57:36 +0300

    feat(data): add LOL download and real image loader

---
+++ src/lowlight_cv/data/__init__.py
@@ -0,0 +1,9 @@
+from lowlight_cv.data.dataset import build_synthetic, load_custom_images, load_real_test_images, load_samples, try_download_lol
+
+__all__ = [
+    "try_download_lol",
+    "build_synthetic",
+    "load_samples",
+    "load_real_test_images",
+    "load_custom_images",
+]
